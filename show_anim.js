e01ebba592bdf1298da20cbd830e0b45828e5e1e
jiawei.hua <jiawei.hua@dianping.com>
2021-11-20 23:25:26 +0800

    love

---
+++ show_anim.js
@@ -0,0 +1,112 @@
+var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(function () {
+    // setup garden
+    $loveHeart = $("#loveHeart");
+    var offsetX = $loveHeart.width() / 2;
+    var offsetY = $loveHeart.height() / 2 - 55;
+    $garden = $("#garden");
+    gardenCanvas = $garden[0];
+    gardenCanvas.width = $("#loveHeart").width();
+    gardenCanvas.height = $("#loveHeart").height()
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter";
+    garden = new Garden(gardenCtx, gardenCanvas);
+
+    // renderLoop
+    setInterval(function () {
+        garden.render();
+    }, Garden.options.growSpeed);
+});
+
+$(window).resize(function() {
+    adjustAvatarPosition();
+    adjustSloganPosition();
+});
+
+function startHeartAnimation() {
+    var interval = 50;
+    var angle = 10;
+    var heart = new Array();
+    var animationTimer = setInterval(function () {
+        var bloom = getHeartPoint(angle);
+        var draw = true;
+        for (var i = 0; i < heart.length; i++) {
+            var p = heart[i];
+            var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+            if (distance < Garden.options.bloomRadius.max * 1.3) {
+                draw = false;
+                break;
+            }
+        }
+        if (draw) {
+            heart.push(bloom);
+            garden.createRandomBloom(bloom[0], bloom[1]);
+        }
+        if (angle >= 30) {
+            clearInterval(animationTimer);
+            showAvatar();
+        } else {
+            angle += 0.2;
+        }
+    }, interval);
+}
+
+function getHeartPoint(angle) {
+    var t = angle / Math.PI;
+    var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
+    var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+    return new Array(offsetX + x, offsetY + y);
+}
+
+function showAvatar() {
+    adjustAvatarPosition();
+    $('.avatar').fadeIn(2000, function() {
+        showSlogan();
+    });
+}
+
+function adjustAvatarPosition() {
+    $('.avatar').css("top", $("#garden").position().top + 155);
+    $('.avatar').css("left", $("#garden").position().left + 215);
+}
+
+function showSlogan() {
+    adjustSloganPosition();
+    $('.slogan').fadeIn(2000, function() {
+        showMessage();
+    });
+}
+
+function adjustSloganPosition() {
+    $('.slogan').css("top", $("#garden").position().top + 285);
+    $('.slogan').css("left", $("#garden").position().left + 215);
+}
+
+function showMessage() {
+    $('.message').fadeIn(3000);
+}
+
+function timeElapse(date){
+    var current = Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    seconds = seconds % 3600;
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var result = "<p>见君心喜，倾盖如故</p> <p><span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒</p>"; 
+    $("#elapseClock").html(result);
+}
